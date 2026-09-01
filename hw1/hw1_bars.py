2f6fbff982785b6a9322e3166c150173b772bd2d
ActuallyMadhav <madhavra.2004@gmail.com>
2026-09-01 14:35:19 -0400

    hw1 task 1

---
+++ hw1/hw1_bars.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+from matplotlib import colors as mcolors
+import argparse
+
+df = pd.read_excel('evs_assignment1.xlsx')
+# print(df.head())
+
+
